21ac93413f3f7c34750f264c4d461172d3667e22
Mudit Bhatt <154462822+themudit-18@users.noreply.github.com>
2024-07-01 02:11:56 +0530

    delegate

---
+++ LAB3/delegate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB3
+{
+    public delegate T CalculatorDelegate<T>(T a, T b);
+    internal class d1
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            return a / b;
+        }
+
+    }
+}
